62fcb252e3493192b4d2b659e01005e104281ceb
bestwebsoft <bestwebsoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-31 16:15:35 +0000

    V1.3 - Added Dutch language file
    git-svn-id: https://plugins.svn.wordpress.org/adsense-plugin/trunk@497999 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adsense-plugin.php
+++ adsense-plugin.php
@@ -4,7 +4,7 @@ Plugin Name: Google AdSense Plugin
 Plugin URI:  http://bestwebsoft.com/plugin/
 Description: This plugin allows implementing Google AdSense to your website.
 Author: BestWebSoft
-Version: 1.2
+Version: 1.3
 Author URI: http://bestwebsoft.com/
 License: GPLv2 or later
 */
