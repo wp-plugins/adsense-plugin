e279752107aaa590516b11ac8655652f0b0a5f2e
bestwebsoft <bestwebsoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-21 16:53:02 +0000

    = V1.12 = Ads displaying on the frontend with Single page option was fixed. We updated all functionality for wordpress 3.5.
    git-svn-id: https://plugins.svn.wordpress.org/adsense-plugin/tags/1.12@642988 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/admin.js
@@ -0,0 +1,338 @@
+jQuery(document).ready(function(){
+			var code;
+			var id;
+			var index_x;
+			var ad_width;
+			var ad_height;
+			var ad_format;
+			var ad_type;
+			var color_border;
+			var color_bg;
+			var color_link;
+			var color_text;
+			var color_url;
+			var client;
+			var width;
+			var height;
+			var format;
+			var type;
+			var border_;
+			var bg;
+			var link;
+			var text;
+			var url;
+			var ui_features;
+			var features;
+			var ad_script;
+			var border_str;
+			var pal_name = ['Default Google pallete','Open Air','Seaside','Shadow','Blue Mix','Ink','Graphite'];
+			var border  = ['#FFFFFF','#FFFFFF','#336699','#000000','#6699CC','#000000','#CCCCCC'];
+			var bgcolor = ['#FFFFFF','#FFFFFF','#FFFFFF','#F0F0F0','#003366','#000000','#CCCCCC'];
+			var linkcol = ['#0000FF','#0000FF','#0000FF','#0000FF','#FFFFFF','#FFFFFF','#000000'];
+			var urlcolor= ['#008000','#008000','#008000','#008000','#AECCEB','#999999','#666666'];
+			var textcol = ['#000000','#000000','#000000','#000000','#AECCEB','#CCCCCC','#333333'];
+			
+			ad_script = '<script type="text/javascript"	src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>'
+			jQuery("#adtypeselect option[value='" + jQuery('#adtypesel_val').val() + "']").attr('selected', 'selected');
+			if (jQuery('#adtypesel_val').val() == 'image_only') {
+				jQuery('#def').css("visibility", "hidden");
+				jQuery('#img_only').css("visibility", "visible");
+			}			
+			
+			jQuery("#corner_style option[value='" + jQuery('#corner_style_val').val() + "']").attr('selected', 'selected');
+			ui_features = jQuery('#corner_style :selected').val();
+			jQuery("#homeAds option[value='" + jQuery('#homeads_val').val() + "']").attr('selected', 'selected');
+			jQuery("input[type=radio][value='" + jQuery('#adtype_val').val() + "']").attr('checked', 'checked');
+			if (jQuery('#adtype_val').val() == 'linkunit') {
+				jQuery('#adtypeselect').attr('disabled', 'disabled');
+				jQuery('#def').css("visibility", "hidden");
+				jQuery('#img_only').css("visibility", "hidden");
+				jQuery('#lnk_unit').css("visibility", "visible");
+				jQuery("#link_unit option[value='" + jQuery('#link_unit_val').val() + "']").attr('selected', 'selected');
+				ad_format = jQuery('#link_unit :selected').val();		
+				index_x = ad_format.indexOf('x');
+				ad_width = ad_format.substring(0, index_x);
+				ad_height = ad_format.slice(index_x+1);	
+			}
+			if (jQuery('#def').css('visibility') == 'visible') {
+				ad_format = jQuery('#default_val').val();
+			}
+			
+			if (jQuery('#img_only').css('visibility') == 'visible') {
+				ad_format = jQuery('#image_only_val').val();
+			}
+
+			if (jQuery('#lnk_unit').css('visibility') == 'visible') {
+				ad_format = jQuery('#link_unit_val').val();
+			}
+			
+			jQuery("#pallete option[value='" + jQuery('#pallete_val').val() + "']").attr('selected', 'selected');
+			jQuery("#position option[value='" + jQuery('#position_val').val() + "']").attr('selected', 'selected');
+			
+			
+			index_x = ad_format.indexOf('x');
+			ad_width = ad_format.substring(0, index_x);
+			ad_height = ad_format.slice(index_x+1);
+			format = "google_ad_format = \"" + ad_format + "_as\";\n";
+			
+
+			curcolor = jQuery('#pallete :selected').val();
+			jQuery.each(pal_name, function(i, val) {
+				if (curcolor == val){
+					jQuery('#Border').val(border[i]);
+					jQuery('#Title').val(linkcol[i]);
+					jQuery('#Background').val(bgcolor[i]);
+					jQuery('#Text').val(textcol[i]);
+					jQuery('#URL').val(urlcolor[i]);
+					jQuery('#Border').css("background", jQuery('#Border').val());		
+					jQuery('#Title').css("background", jQuery('#Title').val());
+					jQuery('#Background').css("background", jQuery('#Background').val());
+					jQuery('#Text').css("background", jQuery('#Text').val());
+					jQuery('#URL').css("background", jQuery('#URL').val());
+				}
+			});	
+			
+			jQuery('#Border').val(jQuery('#border_val').val());
+			jQuery('#Title').val(jQuery('#title_val').val());
+			jQuery('#Background').val(jQuery('#background_val').val());
+			jQuery('#Text').val(jQuery('#text_val').val());
+			jQuery('#URL').val(jQuery('#url_val').val());
+			jQuery('#Border').css("background", jQuery('#Border').val());		
+			jQuery('#Title').css("background", jQuery('#Title').val());
+			jQuery('#Background').css("background", jQuery('#Background').val());
+			jQuery('#Text').css("background", jQuery('#Text').val());
+			jQuery('#URL').css("background", jQuery('#URL').val());
+			
+			
+			jQuery('#pallete').change(function(){
+				curcolor = jQuery('#pallete :selected').val();
+				jQuery.each(pal_name, function(i, val) {
+					if (curcolor == val){
+					jQuery('#Border').val(border[i]);					
+					jQuery('#Title').val(linkcol[i]);					
+					jQuery('#Background').val(bgcolor[i]);					
+					jQuery('#Text').val(textcol[i]);					
+					jQuery('#URL').val(urlcolor[i]);				
+					jQuery('#Border').css("background", jQuery('#Border').val());		
+					jQuery('#Title').css("background", jQuery('#Title').val());
+					jQuery('#Background').css("background", jQuery('#Background').val());
+					jQuery('#Text').css("background", jQuery('#Text').val());
+					jQuery('#URL').css("background", jQuery('#URL').val());					
+					}
+				});	         
+			});			
+			
+			jQuery('#adtypeselect').change(function(){
+					type_ = jQuery('#adtypeselect :selected').val();
+						if ( type_ == 'default' || type_ == 'default_image'){
+							jQuery('#def').css("visibility", "visible");
+							jQuery('#img_only').css("visibility", "hidden");
+							ad_type = jQuery('#adtypeselect :selected').val();
+							ad_format = jQuery('#default :selected').val();		
+							index_x = ad_format.indexOf('x');
+							ad_width = ad_format.substring(0, index_x);
+							ad_height = ad_format.slice(index_x+1);	
+						}
+						if (type_ == 'image_only'){
+							jQuery('#def').css("visibility", "hidden");
+							jQuery('#img_only').css("visibility", "visible");
+							ad_type = jQuery('#adtypeselect :selected').val();		
+							ad_format = jQuery('#image_only :selected').val();		
+							index_x = ad_format.indexOf('x');
+							ad_width = ad_format.substring(0, index_x);
+							ad_height = ad_format.slice(index_x+1);					
+						}
+				});
+				
+				jQuery('#position').change(function(){
+					if (jQuery('#position :selected').val() == 'homepostend' || jQuery('#position :selected').val() == 'homeandpostend' ) {
+						jQuery('#homeAds').removeAttr("disabled");
+						jQuery("#homeAds option[value='" + jQuery('#homeads_val').val() + "']").attr('selected', 'selected');
+					}
+					else {
+						jQuery('#homeAds').attr('disabled', 'disabled');
+						jQuery("#homeAds option[value='1']").attr('selected', 'selected');
+					}
+				});
+					if (jQuery('#position :selected').val() == 'homepostend' || jQuery('#position :selected').val() == 'homeandpostend' ) {
+						jQuery('#homeAds').removeAttr("disabled");
+						jQuery("#homeAds option[value='" + jQuery('#homeads_val').val() + "']").attr('selected', 'selected');
+					}
+					else {
+						jQuery('#homeAds').attr('disabled', 'disabled');
+						jQuery("#homeAds option[value='1']").attr('selected', 'selected');
+					}
+
+
+			jQuery("#ad_type1").click(function() {			
+					jQuery('#adtypeselect').removeAttr("disabled");
+					jQuery('#def').css("visibility", "visible");
+					jQuery('#lnk_unit').css("visibility", "hidden");
+					ad_format = jQuery('#default :selected').val();		
+					index_x = ad_format.indexOf('x');
+					ad_width = ad_format.substring(0, index_x);
+					ad_height = ad_format.slice(index_x+1);
+				});
+				jQuery("#ad_type2").click(function() {	
+					jQuery('#adtypeselect').attr('disabled', 'disabled');
+					jQuery('#def').css("visibility", "hidden");
+					jQuery('#img_only').css("visibility", "hidden");
+					jQuery('#lnk_unit').css("visibility", "visible");								
+					ad_format = jQuery('#link_unit :selected').val();		
+					index_x = ad_format.indexOf('x');
+					ad_width = ad_format.substring(0, index_x);
+					ad_height = ad_format.slice(index_x+1);			
+				});				
+			jQuery('#client_id').val(jQuery('#client_id_val').val());
+			
+			jQuery('#default').change(function(){
+				ad_format = jQuery('#default :selected').val();		
+				index_x = ad_format.indexOf('x');
+				ad_width = ad_format.substring(0, index_x);
+				ad_height = ad_format.slice(index_x+1);
+			});	  
+			
+			jQuery('#image_only').change(function(){
+				ad_format = jQuery('#image_only :selected').val();		
+				index_x = ad_format.indexOf('x');
+				ad_width = ad_format.substring(0, index_x);
+				ad_height = ad_format.slice(index_x+1);
+				format = "google_ad_format = \"" + ad_format + "_as\";\n";
+			});
+			
+			jQuery('#link_unit').change(function(){
+				ad_format = jQuery('#link_unit :selected').val();		
+				index_x = ad_format.indexOf('x');
+				ad_width = ad_format.substring(0, index_x);
+				ad_height = ad_format.slice(index_x+1);
+				format = "google_ad_format = \"" + ad_format + "_0ads_al\";\n";
+			});
+			
+			jQuery('#corner_style').change(function(){
+				if (jQuery('#corner_style :selected').val() != 'none') {
+					ui_features = jQuery('#corner_style :selected').val();
+					features = "google_ui_features = \"rc:" + ui_features + "\";\n";
+				}
+			});
+
+			jQuery(".positive-integer").numeric({ decimal: false, negative: false }, function() { alert("Positive integers only"); this.value = ""; this.focus(); });
+			
+			function arrows() {
+				jQuery('.settings_body').each( function() {
+					if ( jQuery(this).css('display') == 'none' ) {
+					jQuery(this).prev('.settings_head').removeClass('arrow_up');
+					jQuery(this).prev('.settings_head').addClass('arrow_down');
+					}
+				else if ( jQuery(this).css('display') == 'block' ) {
+					jQuery(this).prev('.settings_head').removeClass('arrow_down');
+					jQuery(this).prev('.settings_head').addClass('arrow_up');
+				}
+				});
+			}
+			jQuery("#pos_num").hide();
+			jQuery("#visual").hide();
+			jQuery(".settings_head").click(function(){
+				jQuery(this).next(".settings_body").slideToggle(500);
+				if ( jQuery(this).hasClass('arrow_up') ) {
+					 jQuery(this).removeClass('arrow_up');
+					 jQuery(this).addClass('arrow_down');
+				}
+				else if ( jQuery(this).hasClass('arrow_down') ) {
+					jQuery(this).removeClass('arrow_down');
+					jQuery(this).addClass('arrow_up');
+				}
+			return false;
+			});
+			arrows();
+			
+			
+			jQuery('#donate').val(jQuery('#donate_val').val());
+
+			jQuery("#Border").focus(function() {
+				jQuery('#colorpicker1').farbtastic('#Border');
+				jQuery("#colorpicker1").show();
+				jQuery("#colorpicker2").hide();
+				jQuery("#colorpicker3").hide();
+				jQuery("#colorpicker4").hide();
+				jQuery("#colorpicker5").hide();
+			}).focusout(function () {
+				jQuery("#colorpicker1").hide();
+			});
+			jQuery("#Title").focus(function() {
+				jQuery('#colorpicker2').farbtastic( '#Title' );
+				jQuery("#colorpicker1").hide();
+				jQuery("#colorpicker2").show();
+				jQuery("#colorpicker3").hide();
+				jQuery("#colorpicker4").hide();
+				jQuery("#colorpicker5").hide();
+			}).focusout(function () {
+				jQuery("#colorpicker2").hide();
+			});
+			jQuery("#Background").focus(function() {
+				jQuery('#colorpicker3').farbtastic( '#Background' );
+				jQuery("#colorpicker1").hide();
+				jQuery("#colorpicker2").hide();
+				jQuery("#colorpicker3").show();
+				jQuery("#colorpicker4").hide();
+				jQuery("#colorpicker5").hide();
+			}).focusout(function () {
+				jQuery("#colorpicker3").hide();
+			});
+			jQuery("#Text").focus(function() {
+				jQuery('#colorpicker4').farbtastic( '#Text' );
+				jQuery("#colorpicker1").hide();
+				jQuery("#colorpicker2").hide();
+				jQuery("#colorpicker3").hide();
+				jQuery("#colorpicker4").show();
+				jQuery("#colorpicker5").hide();
+			}).focusout(function () {
+				jQuery("#colorpicker4").hide();
+			});
+			jQuery("#URL").focus(function() {
+				jQuery('#colorpicker5').farbtastic( '#URL' );
+				jQuery("#colorpicker1").hide();
+				jQuery("#colorpicker2").hide();
+				jQuery("#colorpicker3").hide();
+				jQuery("#colorpicker4").hide();
+				jQuery("#colorpicker5").show();
+			}).focusout(function () {
+				jQuery("#colorpicker5").hide();
+			});
+
+			jQuery("#update").click(function () {
+				id = jQuery('#client_id').val();
+				color_border = jQuery('#Border').val();
+				color_link = jQuery('#Title').val();
+				color_bg = jQuery('#Background').val();
+				color_text = jQuery('#Text').val();
+				color_url = jQuery('#URL').val();
+				client = '\ngoogle_ad_client = "pub-' + id + '";\n';
+				width = 'google_ad_width = ' + ad_width + ';\n';
+				height = 'google_ad_height = ' + ad_height + ';\n';
+				if (jQuery("[name=adtype]:radio").filter(":checked").val() == 'adunit') {
+					ad_type = jQuery('#adtypeselect :selected').val();
+					type = 'google_ad_type = "' + ad_type + '";\n';
+					format = 'google_ad_format = "' + ad_format + '_as";\n';
+				}
+				
+				if (jQuery("[name=adtype]:radio").filter(":checked").val() == 'linkunit') {
+					ad_type = jQuery('#adtypeselect :selected').val();
+					type = "";
+					format = 'google_ad_format = "' + ad_format + '_0ads_al";\n';
+				}
+				
+				features = "google_ui_features = \"rc:" + ui_features + "\";\n";
+				if (jQuery('#corner_style :selected').val() == 'none') {
+					features = "";
+				}
+
+				border_ = 'google_color_border = "' + color_border + '";\n';
+				bg = 'google_color_bg = "' + color_bg + '";\n';
+				link = 'google_color_link = "' + color_link + '";\n';
+				text = 'google_color_text = "' + color_text + '";\n';
+				url = 'google_color_url = "' + color_url + '";\n';	
+							
+				code = '<script type="text/javascript">' + client + width + height + format + type + border_ + bg + link + text + url + features +'</script>\n' + ad_script;
+				jQuery('#mycode').val(code);
+			});
+		});
---
+++ readme.txt
@@ -0,0 +1,163 @@
+=== Google AdSense Plugin ===
+Contributors: bestwebsoft
+Donate link: https://www.2checkout.com/checkout/purchase?sid=1430388&quantity=10&product_id=13
+Tags: google, adsense, ads, bestwebsoft, google adsense, google plugin, adsense plugin, ads plugin, gogle
+Requires at least: 2.9
+Tested up to: 3.5
+Stable tag: 1.12
+
+This plugin allows implementation of Google AdSense to your website.
+
+== Description ==
+
+Google AdSense Plugin allows setting of ads displaying parameters, such as format (text ad, image, text with an image or as links), size, color of the elements at the ad block, rounded corners and also position of the ad block at the website.
+
+<a href="http://wordpress.org/extend/plugins/adsense-plugin/faq/" target="_blank">FAQ</a>
+<a href="http://bestwebsoft.com/plugin/google-adsense-plugin/" target="_blank">Support</a>
+
+= Features =
+
+* Display: Ability to adjust look of the ad, choose a color scheme, layout, number of ad units per page.
+* Display: Ability to insert google ads in widget.
+* Actions: Ability to insert google ads to the website automatically.
+
+= Translation =
+
+* Brazilian Portuguese (pt_BR) (thanks Rafael)
+* Dutch (nl_NL) (thanks to Paul Göttgens)
+* Hebrew (he_IL) (thanks to Ahrale)
+* Polish (pl_PL) (thanks to Tomasz Łaziuk)
+* Spanish (es_ES) (thanks to José Fernando De Leon)
+* Russian (ru_RU)
+
+If you create your own language pack or update an existing one, you can send <a href="http://codex.wordpress.org/Translating_WordPress" target="_blank"> text in PO and MO files</a> for <a href="http://bestwebsoft.com/" target="_blank">BWS</a> and we'll add it to the plugin. You can download the latest version of the program for work with PO and MO files  <a href="http://www.poedit.net/download.php" target="_blank">Poedit</a>.
+
+= Technical support =
+
+Dear users, our plugins are available for free download. If you have any questions or propositions regarding functionality of our plugins (current options, new options, current issues) please feel free to contact us. Please note that we accept requests in English language only. All messages on another languages wouldn't be accepted. 
+
+Also, emails which are reporting about plugin's bugs are accepted for investigation and fixing. Your request must contain URL of the website, issues description and WordPress admin panel access. Plugin customization based on your Wordpress theme is a paid service (standard price is $10, but it could be higer and depends on the complexity of requested changes). We will analize existing issue and make necessary changes after 100% pre-payment.All these paid changes and modifications could be included to the next version of plugin and will be shared for all users like an integral part of the plugin. Free fixing services will be provided for user who send translation on their native language (this should be a new translation of a certain plugin, and you can check available translations on the official plugin page).
+
+== Installation ==
+
+1. Upload `adsense-plugin` folder to the `/wp-content/plugins/` directory.
+2. Activate plugin through the 'Plugins' menu in WordPress.
+3. Plugin settings are located in "BWS Plugin", "AdSense".
+
+== Frequently Asked Questions ==
+
+= How to change Publisher ID =
+
+Go to the Settings page and change value for the 'Publisher ID' field.
+
+= How to change position of the ad block =
+
+At the page of plugin settings open menu item 'Position & Numbers of Ads' and on 'Position' list choose appropriate for you position.
+
+== Screenshots ==
+
+1. AdSense Settings page.
+2. All menu items at the page are unwrapped.
+
+== Changelog ==
+
+= V1.12 - 21.12.2012 =
+* Bugfix : Ads displaying on the frontend with Single page option was fixed.
+* Update : We updated all functionality for wordpress 3.5.
+
+= V1.11 - 26.11.2012 =
+* NEW : The option for the displaying of google ads has been implemented using the widget.
+
+= V1.10 - 03.08.2012 =
+* NEW : Polish and Spanish languages files was added to the plugin.
+
+= V1.9 - 24.07.2012 =
+* Bugfix : Cross Site Request Forgery bug was fixed. 
+
+= V1.8 - 09.07.2012 =
+* NEW : Hebrew language file is added to the plugin.
+* Bugfix : Ads displaying on the frontend with Single page and Home page option was fixed. 
+
+= V1.7 - 27.06.2012 =
+* Update : We updated all functionality for wordpress 3.4.
+
+= V1.6 - 11.06.2012 =
+* NEW : Brazilian Portuguese file is added to the plugin.
+
+= V1.5 - 12.03.2012 =
+* Change :  The name of a variable for plugin options is replaced with unique name so that the name of this variable is not in common with names of variables of other plugins and wordpress.
+
+= V1.4 - 24.02.2012 =
+* Change : Code that is used to connect styles and scripts is added to the plugin for correct SSL verification.
+
+= V1.3 - 31.01.2012 =
+* NEW : Dutch language file is added.
+
+= V1.2 - 27.01.2012 =
+* Bugfix : Settings and ads displaying are saved on the frontend. 
+
+= V1.1 - 29.12.2011 =
+* Changed : BWS plugin's section. 
+* Changed : Default plugin's settings.
+
+= V1.0 - 27.12.2011 =
+* NEW : All words are added to the language file.
+
+= V0.53 - 24.11.2011 =
+* NEW : Settigns for color shift of the elements at the ad block are added.
+
+= V0.52 =
+* NEW : Parameters of ads display are added.
+
+= V0.51 =
+* Changed : Usability at the settings page of the plugin was improved.
+
+== Upgrade Notice ==
+
+= V1.12 =
+Ads displaying on the frontend with Single page option was fixed. We updated all functionality for wordpress 3.5.
+
+= V1.11 =
+The option for the displaying of google ads has been implemented using the widget.
+
+= V1.10 =
+Polish and Spanish languages files was added to the plugin.
+
+= V1.9 - 24.07.2012 =
+Cross Site Request Forgery bug was fixed. 
+
+= V1.8 =
+Hebrew language file is added to the plugin. Ads displaying on the frontend with Single page and Home page option was fixed. 
+
+= V1.7 =
+We updated all functionality for wordpress 3.4.
+
+= V1.6 =
+Brazilian Portuguese file is added to the plugin.
+
+= V1.5 =
+The name of a variable for plugin options is replaced with unique name so that the name of this variable is not in common with the names of the variables of other plugins and wordpress.
+
+= V1.4 =
+Code that is used to connect styles and scripts is added to the plugin for correct SSL verification.
+
+= V1.3 =
+Dutch language file is added.
+
+= V1.2 =
+A bug with saving settings and ads displaying on the frontend was fixed in this version. Please upgrade plugin immediately. Thank you
+
+= V1.1 - 29.12.2011 =
+BWS plugin's section was changed. Default plugin's settings were changed. 
+
+= V1.0 - 27.12.2011 =
+All words are added in language file.
+
+= V0.53 =
+Settigns for color shift of the elements at the ad block are added.
+
+= V0.52 =
+Ads displaying parameters are added.
+
+= V0.51 =
+Usability of the plugin's settings page was improved.
